208bf4ad59741274739f4ba47f00f9c3501971eb
silvano valdez <silvanovaldez90@gmail.com>
2019-09-05 10:04:53 -0500

    sissss

--- helper/urls.py
+++ helper/urls.py
@@ -8,13 +8,11 @@ app_name = 'help'
 
 urlpatterns = [
     path('', views.add_auto,  name='home'),
-    path('car-unlock-service/', views.car_unlock, name='unlock_car'),
-    path('jump-start-car/', views.battery_jump_start, name='jump_start'),
+    path('Car-Unlock-Service-Chicago-IL/', views.car_unlock, name='unlock_car'),
+    path('Jump-Start-Service-Illinois-Chicago/', views.battery_jump_start, name='jump_start'),
     path('Pasar-Cables-Chicago-IL/', views.jump_service_spanish, name='spanish'),
-    path('undo/', views.undo_all, name='undo'),
-    path('add/',views.add_indi, name='add'),
     path('thankyou/', views.request_services, name='thankyou'),
-    path('sitemap.xml', views.site_map, name='sitemap'),
+    # path('sitemap.xml', views.site_map, name='sitemap'),
     path('robots.txt', TemplateView.as_view(template_name="helper/robots.txt", content_type="text/plain"),
          name="robots_file")
 
