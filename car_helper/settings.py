c688d9735f25686d04f387cbb2a839bab1daee45
silvano valdez <silvanovaldez90@gmail.com>
2019-09-21 18:30:38 -0500

    sissss

--- car_helper/settings.py
+++ car_helper/settings.py
@@ -59,13 +59,13 @@ MIDDLEWARE = [
 
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
+   'default': {
+      'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+      'LOCATION': 'https://www.chicagocarhelp.com',
+   }
 }
 
-CACHE_MIDDLEWARE_SECONDS= 20000
+CACHE_MIDDLEWARE_SECONDS = 20000
 
 ROOT_URLCONF = 'car_helper.urls'
 
